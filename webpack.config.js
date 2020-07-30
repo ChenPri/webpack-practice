07193d632494b704e3da16da8924d2d2471db641
= <=>
2020-07-30 12:48:18 +0800

    feat: Loading CSS

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,17 @@ module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js'
+        filename: 'bundle.js'
+    },
+    module:{
+        rules:[
+           {
+            test: /\.css$/,
+            use:[
+            'style-loader',
+            'css-loader'
+            ]
+           }
+        ]
     }
 }
